62ef54440676e0692bf4fdd2566cc307881aa4ba
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-10 00:39:56 +0530

    version 6.0

---
+++ dev_if_else - Copy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main()
+{
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+
+	if(a==b){
+		printf("\nyou are right the number are true");
+	}else if(a>b){
+		printf("\nyou are wrong the a is greater than b");
+	}else{
+		printf("\nthe numbers are not equal");
+	}
+	
+	int f,g,h;
+	scanf("%d",&f);
+//		scanf("%d",&f);
+	scanf("%d",&g);
+	scanf("%d",&h);
+	
+	printf("%d",f+g+h);
+
+	
+	
+	
+}
