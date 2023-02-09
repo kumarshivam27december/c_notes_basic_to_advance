be3b927d8dad0009ea9a43f00702ce66822d6569
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-10 00:08:42 +0530

    version 3.0

---
+++ dev_hello_world.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void main()
+{
+//	printf("\nhello machine world");
+
+
+
+
+//	int q,w,e,r,t,y,u,i,o,p;
+
+int q;
+
+
+//	scanf("\n please The value of q = %d",q);
+//	scanf("\n please The value of w = %d",&w);
+//	scanf("\n please The value of e = %d",&e);
+//	scanf("\n please The value of r = %d",&r);
+//	scanf("\n please The value of t = %d",&t);
+//	scanf("\n please The value of y = %d",&y);
+//	scanf("\n please The value of u = %d",&u);
+//	scanf("\n please The value of i = %d",&i);
+//	scanf("\n please The value of o = %d",&o);
+//	scanf("\n please The value of p = %d",&p);
+
+//	printf("%d",q+w+e+r+t+y+u+i+o+p);
+scanf("%d",&q);
+printf("%d",q+5);
+	
+}
