ce38a4bf276300504585a456004f2064b0765b01
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-11 19:28:34 +0530

    version 11.0

---
+++ bhaiya_explanation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+	int a=5,b=10,c;
+//	int c= (a++)++;
+//	printf("%d",c);
+c=a+1;
+unsigned long long d=10987654320987654321;
+unsigned long long e=10987654320987654321;
+
+
+    printf("%d",d+e);
+}
