bd1592bb4af2a7a100119fa2f858a7c3f3aa5043
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-11 19:09:10 +0530

    version 7.0

---
+++ ternary_operator_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+	int a=5,b=10;
+	/*int a=5,b=10;
+	int c = a>=b?a:b;*/
+	a>=b?printf("Welcome"):printf("Bye");  //bye will be the output
+//	printf()
+//int c=printf("welcome"); // if no character then 0 then c value will be zero
+
+//printf("value of c = %d",c);  //7
+//printf("welcome")?printf("welcome"):printf("bye");// if welcome instead zero is there then it will just print bye because nothing is present in the first printf function
+//printf(" ")?printf("welcome"):printf("bye"); //welcome will be printed only
+//printf("")?printf("welcome"):printf("bye"); // bye will be the output
+
+}
