55843db7c8c67fe7382331163e4ed255136b9ccb
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-15 02:38:12 +0530

    version 18.0

---
+++ C Program To Find Area And Circumference Of Circle.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+	float a;
+	scanf("%f",&a);
+	float pi=3.14159265359;
+	float circumference=2*pi*a;
+    float area=pi*a*a;
+	printf("%f",circumference);
+	printf("\n%f",area);
+
+	
+}
--- Print the ASCII Value of the Character.c
+++ Print the ASCII Value of the Character.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 void main()
 {
-	
-	int a=5,b=10;
-	int c=a<<2<<3;
+	char c;
+	scanf("%c",&c);
 	printf("%d",c);
-//	printf("%d",d);
 }
