e3f3f43c9ca9e5855e694ab2729b241f80397520
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-16 14:06:21 +0530

    version 20.0

---
+++ dev_2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	int a,b,c,d,e,f,g,h,i,j,k,l;
+	a=1;
+	b=2;
+	c=3;
+	d=4;
+	e=5;
+	f=6;
+	g=7;
+	h=8;
+	i=a+b+c+d+e+f+g+h;
+	printf("%d",i);
+}
