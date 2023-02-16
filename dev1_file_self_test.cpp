1c350193174405d57f55b8a9bc302ce491f10758
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-16 23:14:02 +0530

    version 27.0

---
+++ dev1_file_self_test.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+	int a,s,d,l;
+	a=90;
+	s=80;
+	d=a+s;
+	printf("%d\n",d);
+	printf(" are you really stupid! or just acting\n");
+	scanf("%d",l);
+	printf("%d",l+5);
+	
+	return 0;
+}
