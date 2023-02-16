24353066513c4a977c4e1c4144a089e940385fc9
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-16 23:41:44 +0530

    version 35.0

---
+++ if_else.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+      int a;
+      scanf("%d",&a);
+      
+	 	
+	/*
+	
+                                 //	if and else
+     if(exp) //TEST=> true / false                            
+	 {
+	 statement(s);
+	 }
+	*/
+	if(a>=0)
+	{
+	  printf("Positive number");	
+	}
+	
+	// if is independent of else
+	// but else is dependent on if statement
+}
