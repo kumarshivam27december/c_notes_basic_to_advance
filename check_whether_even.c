8783a56d494115effeaf8bfcfbadf5c7b20275da
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-18 17:14:02 +0530

    version 45.0

---
+++ check_whether_even.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	printf("the number is = %d",a);
+	a>=0?printf("positive"):printf("negative");
+	
+	
+	//input = A,a,1,-5,0,
+}
