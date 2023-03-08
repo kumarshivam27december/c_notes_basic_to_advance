d820564cbcec3c4e463007a0ab5245ddd1ecdc75
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-03-08 20:50:48 +0530

    version 96.0

---
+++ Calculate the Sum of Natural Numbers Using the While Loop2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+	//using formula to  find sum of n natural number
+	int input,total;
+	printf("ENTER THE USER INPUT = ");
+	scanf("%d",&input);
+	total=(input*(input+1))/2;
+	printf("THE SUM OF THE NATURAL NUMBER = %d",total);
+	
+		
+	
+	return 0;
+}
