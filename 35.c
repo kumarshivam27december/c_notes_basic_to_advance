96d5fde195f2a5bf213ec40b497aee12bc8ba879
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-03-16 22:44:49 +0530

    version 113.0

---
+++ 35.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ void main()
+{
+	char ch;
+	printf("Press any key to continue");
+	ch = getch();
+	printf(" you pressed:");
+	putch(ch);
+}
+
---
+++ 36.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main()
+{
+	char ch[10];
+	gets(ch);
+//	puts(ch);
+}
