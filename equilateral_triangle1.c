c00904f28e69f76b4cdc3174d2e09509a99c838e
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-03-19 01:05:22 +0530

    version 115.0

---
+++ equilateral_triangle1.c
@@ -0,0 +1,39 @@
+/*
+     *
+    ***
+   *****
+  *******
+ *********
+*********** 
+*/
+
+#include<stdio.h>
+void main()
+{
+	int r,c,space,n; 	
+	printf("\nEnter number of line = ");
+	scanf("%d",&n);
+	for(r = 1; r <= n; r++)//line control or Row control
+	{
+		// Pattern 2 
+		//First print space in the same row or line 
+		for(space = n; space>=r; space--) //Space Control 
+		{
+			printf(" ");  // Space Control  
+		}	
+		
+		// Pattern 1 
+		// Than print the value in the same row
+		for(c = 1; c<=2*r-1; c++) //value control loop 
+		{
+			if(c%2==0)
+			 printf("0");  // Value 
+			else
+			 printf("1");
+		}	
+		printf("\n");   // New Line
+	}
+	
+}
+
+
