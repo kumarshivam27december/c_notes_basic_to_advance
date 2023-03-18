9ea9e3eb169fd7eb7ac7be795bbaccf230b40358
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-03-19 01:32:21 +0530

    version 116.0

---
+++ eq_triangle5.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+void main(){int i,j,space,input;printf("Enter the number of lines = ");scanf("%d",&input);for(i=1;i<=input;i++){for(space=input;space>=i;space--){printf(" ");}for(j=1;j<=2*i-1;j++){printf("$");}printf("\n");}}
+
+		
+
+
---
+++ triangle_eql3.c
@@ -0,0 +1,46 @@
+/*
+  Pattern Printing Program
+Pattern 3:
+
+     *
+    ***
+   *****
+  *******
+ *********
+***********
+  
+  
+  
+*/
+
+#include<stdio.h>
+void main()
+{
+	int r,c,space,n; 	
+	printf("\nEnter number of line = ");
+	scanf("%d",&n);
+	for(r = 1; r <= n; r++)//line control or Row control
+	{
+		// Pattern 2 
+		//First print space in the same row or line 
+		for(space = n; space>=r; space--) //Space Control 
+		{
+			printf(" ");  // Space Control  
+		}	
+		
+		// Pattern 1 
+		// Than print the value in the same row
+		for(c = 1; c<=2*r-1; c++) //value control loop 
+		{
+			if(c%2==0)
+			 printf("*");  // Value 
+			else
+			 printf(" ");
+//              printf("$");
+		}	
+		printf("\n");   // New Line
+	}
+	
+}
+
+
