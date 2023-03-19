88d494743d8ea8a1a189e1cc41d439ff08f434a5
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-03-19 16:43:35 +0530

    version 117.0

---
+++ eq_tri_1.c
@@ -0,0 +1,22 @@
+/*
+print the following pattern
+                 *
+                ***
+               *****
+
+*/
+#include <stdio.h>
+void main(){
+	int i,j,input,space;
+	printf("Enter the number of lines = ");
+	scanf("%d",&input);
+	for(i=1;i<=input;i++){
+		for(space=input;space>=i;space--){
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++){
+			printf("*");
+		}
+		printf("\n");
+	}
+}
