3dc1000500bb999ce0ef9a18a8298363bf5158d2
Shivam Kumar <114799228+kumarshivam27december@users.noreply.github.com>
2023-05-17 16:22:38 +0530

    Add files via upload

--- armstrong1.c
+++ armstrong1.c
@@ -1,20 +1,34 @@
-#include<stdio.h>  
- int main()    
-{    
-int n,r,sum=0,temp;    
-printf("enter the number=");    
-scanf("%d",&n);    
-temp=n;    
-while(n>0)    
-{    
-r=n%10;    
-sum=sum+(r*r*r);    
-n=n/10;    
-}    
-if(temp==sum)    
-printf("armstrong  number ");    
-else    
-printf("not armstrong number");    
-return 0;  
-}   
+#include <stdio.h>
+#include <math.h>
+void main(){
+	int input, temp, rem;
+	int sum = 0;
+	int n = 0;
+	printf("Enter the number to check if Armstrong or not: ");
+	scanf("%d", &input);
+	temp = input;
+	// Count the number of digits in the input
+	while (temp != 0)
+	{
+		temp = temp / 10;
+		n++;
+	}
+	temp = input;
+	// Calculate the sum of the cubes of each digit
+	while (temp != 0)
+	{
+		rem = temp % 10;
+		sum += pow(rem, n);
+		temp /= 10;
+	}
+	// Check if the input is Armstrong or not
+	if (sum == input)
+	{
+		printf("%d is an Armstrong number.\n", input);
+	}
+	else
+	{
+		printf("%d is not an Armstrong number.\n", input);
+	}
+}
 
